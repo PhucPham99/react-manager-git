3d3592c355dad1c67df684374ef4ca7415297bcf
PhucPham99 <strike.beast.1@gmail.com>
2023-11-22 21:06:55 +0700

    branch master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import  {Routes, Route, Link} from 'react-router-dom'
+import './component/bmi'
+import './component/gallery'
+import './component/chuyendoidodai'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+        <Link to="/bmi">BMI</Link>
+        <Link to="/gallery">gallery</Link>
+        <Link to="/chuyendoidodai">chuyendoidodai</Link>
     </div>
   );
 }
